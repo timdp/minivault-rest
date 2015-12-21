bc7d5ce322e8008fc6f1bbb8b6c269b237ca8c5a
Tim De Pauw <tim.depauw@zentrick.com>
2015-12-21 18:24:26 +0100

    Removing tests again; linting with Standard as test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,6 @@
 
 import gulp from 'gulp'
 import loadPlugins from 'gulp-load-plugins'
-import {Instrumenter} from 'isparta'
 import del from 'del'
 import mkdirp from 'mkdirp'
 import seq from 'run-sequence'
@@ -15,15 +14,9 @@ const plumb = () => $.plumber({
   errorHandler: $.notify.onError('<%= error.message %>')
 })
 
-const test = () => {
-  return gulp.src(['test/lib/setup.js', 'test/unit/**/*.js'], {read: false})
-    .pipe(plumb())
-    .pipe($.mocha({reporter: 'dot'}))
-}
-
 gulp.task('clean', () => del.sync(DEST))
 
-gulp.task('build', ['test'], () => {
+gulp.task('build', () => {
   mkdirp.sync(DEST)
   return gulp.src('src/**/*.js')
     .pipe(plumb())
@@ -33,22 +26,6 @@ gulp.task('build', ['test'], () => {
 
 gulp.task('cleanbuild', cb => seq('clean', 'build', cb))
 
-gulp.task('coverage', cb => {
-  gulp.src('src/**/*.js')
-    .pipe(plumb())
-    .pipe($.istanbul({instrumenter: Instrumenter}))
-    .pipe($.istanbul.hookRequire())
-    .on('finish', () => test().pipe($.istanbul.writeReports()).on('end', cb))
-})
-
-gulp.task('coveralls', ['coverage'], () => {
-  return gulp.src('coverage/lcov.info')
-    .pipe(plumb())
-    .pipe($.coveralls())
-})
-
-gulp.task('test', test)
-
-gulp.task('watch', () => gulp.watch('{src,test}/**/*', ['cleanbuild']))
+gulp.task('watch', () => gulp.watch('src/**/*', ['cleanbuild']))
 
 gulp.task('default', ['cleanbuild'], () => gulp.start('watch'))
